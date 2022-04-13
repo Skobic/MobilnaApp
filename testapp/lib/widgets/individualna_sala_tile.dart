d2b313c8948f2393870d7cc431395cdc6a224947
blagojevicNikola <nikolablagojevic848@gmail.com>
2022-04-13 19:13:25 +0200

    Pocetne izmjene na pregledu sala i citaonica

---
+++ testapp/lib/widgets/individualna_sala_tile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../pocetna_strana.dart';
+
+class IndividualnaSalaTile extends StatelessWidget {
+  const IndividualnaSalaTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: ListTile(
+        tileColor: const Color.fromARGB(255, 245, 245, 245),
+        onTap: () {
+          navigatorKey.currentState!.pushNamed('pregled_individualne_sale');
+        },
+        title: const Text('Sala',
+            style: TextStyle(
+                color: Color.fromARGB(255, 86, 86, 86),
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),
+        trailing: Row(mainAxisSize: MainAxisSize.min, children: const [
+          Text('3',
+              style: TextStyle(
+                  fontSize: 17, color: Color.fromARGB(255, 116, 116, 116))),
+          SizedBox(width: 5),
+          Icon(
+            Icons.person,
+            size: 17,
+            color: Color.fromARGB(255, 116, 116, 116),
+          ),
+          SizedBox(width: 10),
+          Icon(Icons.arrow_forward_ios, color: Color.fromARGB(255, 86, 86, 86))
+        ]),
+      ),
+    );
+  }
+}
